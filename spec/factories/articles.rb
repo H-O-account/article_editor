e9e59c2666aabe09de44a90d8e0ac314b4c2e530
Hide <>
2023-02-21 22:36:09 +0900

    記事の下書き機能の追加

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,26 +1,8 @@
-# == Schema Information
-#
-# Table name: articles
-#
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
-#
-# Indexes
-#
-#  index_articles_on_user_id  (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
-#
 FactoryBot.define do
   factory :article do
     title { Faker::Lorem.characters(number: 1..16) }
     body { Faker::Lorem.characters(number: 1..16) }
     user # association :user の省略形
+    status { 0 }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: articles
-#
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
-#
-# Indexes
-#
-#  index_articles_on_user_id  (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
-#
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
@@ -24,6 +5,23 @@ RSpec.describe Article, type: :model do
     it "投稿される" do
       article = build(:article)
       expect(article).to be_valid
+    end
+  end
+
+  context "下書きとして保存する場合" do
+    let(:draft) { build(:article, status: 0) }
+    it "statusの値が0になる" do
+      expect(draft).to be_valid
+      expect(draft.status).to eq "draft"
+    end
+  end
+
+  context "公開して保存する場合" do
+    let(:published) { build(:article, status: 1) }
+    it "statusの値が1になる" do
+      subject
+      expect(published).to be_valid
+      expect(published.status).to eq "published"
     end
   end
 end
